42be0893dc8669ca9e20eaaa77e8071fe544f9a2
BrotherhoodOfHam <thomas.street@florimont.com>
2018-02-16 21:50:10 +0000

    Implemented bicubic sampling.

--- src/Samplers.h
+++ src/Samplers.h
@@ -10,38 +10,29 @@
 /*
 	UV coordinates in 2D
 
-	each component is 0.0f-1.0f
+	Each component is between 0.0f->1.0f
 */
 struct UV
 {
-	//image coordinates
 	float u;
 	float v;
-	//image coordinate derivatives
-	float ddu;
-	float ddv;
 
-	UV(float _u = 0.0f, float _v = 0.0f, float _ddu = 0.0f, float _ddv = 0.0f) :
-		u(_u), v(_v), ddu(_ddu), ddv(_ddv)
+	UV(float _u = 0.0f, float _v = 0.0f) :
+		u(_u), v(_v)
 	{}
 };
 
 /*
-	UV coordinates in 3D
+	UVW coordinates in 3D
 */
 struct UVW
 {
-	//image coordinates
 	float u;
 	float v;
 	float w;
-	//image coordinate derivatives
-	float ddu;
-	float ddv;
-	float ddw;
 
-	UVW(float _u = 0.0f, float _v = 0.0f, float _w = 0.0f, float _ddu = 0.0f, float _ddv = 0.0f, float _ddw = 0.0f) :
-		u(_u), v(_v), w(_w), ddu(_ddu), ddv(_ddv), ddw(_ddw)
+	UVW(float _u = 0.0f, float _v = 0.0f, float _w = 0.0f) :
+		u(_u), v(_v), w(_w)
 	{}
 };
 
@@ -54,8 +45,8 @@ public:
 
 	static Volume::ElementType sample(const VolumeSubimage& view, const UV& coords)
 	{
-		const auto _u = (Volume::IndexType)(coords.u * (float)view.width());
-		const auto _v = (Volume::IndexType)(coords.v * (float)view.height());
+		const auto _u = (Volume::IndexType)(coords.u * (float)view.width()) % view.width();
+		const auto _v = (Volume::IndexType)(coords.v * (float)view.height()) % view.height();
 
 		return view.at(_u, _v);
 	}
@@ -86,14 +77,14 @@ public:
 			g = (u,v)
 		*/
 
-		//Calculate nearest 4 neighbouring texel coordinates
-		float xmin = floorf((coords.u) * (float)view.width());
-		float xmax = ceilf((coords.u) * (float)view.width());
-		float ymin = floorf((coords.v) * (float)view.height());
-		float ymax = ceilf((coords.v) * (float)view.height());
-
 		float x = coords.u * view.width();
 		float y = coords.v * view.height();
+
+		//Calculate nearest 4 neighbouring texel coordinates
+		float xmin = floorf(x);
+		float xmax = ceilf(x);
+		float ymin = floorf(y);
+		float ymax = ceilf(y);
 
 		//Ensure coordinates are in bounds
 		xmin = std::max(xmin, 0.0f);
@@ -126,14 +117,86 @@ public:
 
 /*
 	Bicubic sampler
+
+	formula derived from this example: http://www.paulinternet.nl/?page=bicubic
 */
 class BicubicSampler
 {
+private:
+
+	//Perform cubic interpolation on 4 control points
+	static float interp(float p[4], double i)
+	{
+		return p[1] + 0.5 * i * (p[2] - p[0] + i*(2.0f*p[0] - 5.0f * p[1] + 4.0f * p[2] - p[3] + i * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
+	}
+
 public:
 
-	static Volume::ElementType sample(const VolumeSubimage& image, const UV& coords)
+	static Volume::ElementType sample(const VolumeSubimage& view, const UV& coords)
 	{
-		
+		/*
+			16 control points a->p
+
+			a----b----c----d
+			|    |    |    |
+			|    |    |    |
+			e----f----g----h
+			|    |    |    |
+			|    |    |    |
+			i----j----k----l
+			|    |    |    |
+			|    |    |    |
+			m----n----o----p
+		*/
+
+		//Convert normalized uv's into coordinates relative to the image we sample
+		float x = coords.u * view.width();
+		float y = coords.v * view.height();
+
+		float xmin = floorf(x);
+		float xmax = ceilf(x);
+		float ymin = floorf(y);
+		float ymax = ceilf(y);
+
+		const float bias = std::numeric_limits<float>::epsilon();
+
+		//Compute texcoord gradient - bias prevents divide by zero errors
+		float xgradient = (x - xmin) / (bias + (xmax - xmin));
+		float ygradient = (y - ymin) / (bias + (ymax - ymin));
+
+		//const float xgradient = x - xmin;
+		//const float ygradient = y - ymin;
+
+		//Sample within bounds - helper function
+		auto sm = [&](size_t u, size_t v)->float
+		{
+			//Ensure uv's are in bounds when sampling
+			u = std::max(std::min(u, view.width() - 1), (size_t)0);
+			v = std::max(std::min(v, view.height() - 1), (size_t)0);
+			return (float)view.at(u, v);
+		};
+
+		//Fetch values from control points
+		float points[4][4] =
+		{
+			//1. row a->d
+			{ sm(xmin - 1, ymin - 1), sm(xmin, ymin - 1), sm(xmax, ymin - 1), sm(xmax + 1, ymin - 1) },
+			//2. row e->h
+			{ sm(xmin - 1, ymin),     sm(xmin, ymin),     sm(xmax, ymin),     sm(xmax + 1, ymin) },
+			//3. row i->l
+			{ sm(xmin - 1, ymax),     sm(xmin, ymax),     sm(xmax, ymax),     sm(xmax + 1, ymax) },
+			//4. row m->p
+			{ sm(xmin - 1, ymax + 1), sm(xmin, ymax + 1), sm(xmax, ymax + 1), sm(xmax + 1, ymax + 1) },
+		};
+
+		float results[] = {
+			interp(points[0], xgradient),
+			interp(points[1], xgradient),
+			interp(points[2], xgradient),
+			interp(points[3], xgradient)
+		};
+
+		return interp(results, ygradient);
 	}
 };
 
